9a543495a1fa5a7184370397b20b6273f55b7961
kiranprasai <roundkiran@gmail.com>
2025-02-05 18:32:57 +0545

    Update news.js

--- news.js
+++ news.js
@@ -1,3 +1,14 @@
-fetch("https://apinp.com/news/api.php")
-    .then(response => response.json())
-    .then(data => console.log(data));
+ <script>
+        fetch("https://apinp.com/news/api.php")
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Network response was not ok");
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                document.body.innerHTML = "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+            })
+            .catch(error => console.error("Error fetching data:", error));
+    </script>
